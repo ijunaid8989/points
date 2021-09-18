a77777d94f1394e80f7e19f98887edf1393a2809
Junaid Farooq <junaid@evercam.io>
2021-09-18 20:01:10 +0500

    Added Users model && migration with min/max validation of points.

---
+++ lib/remote/users.ex
@@ -0,0 +1,17 @@
+defmodule Users do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :points, :integer
+
+    timestamps(inserted_at: :created_at, type: :utc_datetime_usec, default: DateTime.utc_now())
+  end
+
+  def changeset(model, params \\ :invalid) do
+    model
+    |> cast(params, [:points])
+    |> validate_required([:points])
+    |> validate_number(:points, greater_than_or_equal_to: 0, less_than_or_equal_to: 100)
+  end
+end
