e4bdb697ca887c59f61d3657854d23fef7063023
Junaid Farooq <junaid@evercam.io>
2021-09-19 14:03:23 +0500

    Start running GenServer on startup.

--- lib/remote/application.ex
+++ lib/remote/application.ex
@@ -14,7 +14,8 @@ defmodule Remote.Application do
       # Start the PubSub system
       {Phoenix.PubSub, name: Remote.PubSub},
       # Start the Endpoint (http/https)
-      RemoteWeb.Endpoint
+      RemoteWeb.Endpoint,
+      Remote.Points
       # Start a worker by calling: Remote.Worker.start_link(arg)
       # {Remote.Worker, arg}
     ]
