a64290a570e4eba74cf7228d6509853c55167581
Junaid Farooq <junaid@evercam.io>
2021-09-19 07:24:03 +0500

    Change the chunk size for insertion.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -3,7 +3,7 @@ alias Remote.Repo
 datetime = DateTime.utc_now()
 
 Enum.map(1..1_000_000, fn _x -> %{points: 0, inserted_at: datetime, updated_at: datetime} end)
-|> Enum.chunk_every(6000)
+|> Enum.chunk_every(21845)
 |> Enum.each(fn rows ->
   Ecto.Multi.new()
   |> Ecto.Multi.insert_all(:insert_all, Users, rows)
