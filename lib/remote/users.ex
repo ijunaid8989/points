c91da066a24280616478bcd150a3c60d23793f06
Junaid Farooq <junaid@evercam.io>
2021-09-19 10:46:35 +0500

    Updated all users with random number points.

--- lib/remote/users.ex
+++ lib/remote/users.ex
@@ -1,11 +1,17 @@
 defmodule Users do
   use Ecto.Schema
   import Ecto.Changeset
+  import Ecto.Query
 
   schema "users" do
     field :points, :integer
 
     timestamps(type: :utc_datetime_usec, default: DateTime.utc_now())
+  end
+
+  def update() do
+    update(Users, set: [points: fragment("floor(random()*100)")])
+    |> Remote.Repo.update_all([])
   end
 
   def changeset(model, params \\ :invalid) do
